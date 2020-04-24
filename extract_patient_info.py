8369d7a1ae0093c476c69d08aaba537c089beefa
Yousun Ko <ko.yousun82@gmail.com>
2020-04-24 12:07:57 +0900

    함수명 수정, folder당 dcm 파일 하나에서만 추출 추가, dicom_root_path 선택 GUI로 추가

--- extract_patient_info.py
+++ extract_patient_info.py
@@ -9,48 +9,83 @@ import PIL
 import numpy as np
 import shutil
 
+
 # config
-dicom_root_path = "./dicom_sample"  # 경로형태로 넣어줘야 함
-output_jpeg_dir = 'output_jpeg/' + datetime.now().strftime("%y%m%d") + "_" + os.path.basename(dicom_root_path)  # root 경로에 있는 디렉토리 이름으로 넣어줘야 함
+nowdate = datetime.now().strftime('%y%m%d')
+
+# GUI로 dicom_root_path 선택
+from tkinter import filedialog
+from tkinter import *
+root_path = Tk()
+root_path.dirName = filedialog.askdirectory()
+dicom_root_path = root_path.dirName
+
+# root 경로 입력
+# dicom_root_path = '/Users/Yousun/Downloads/TEST'
+
+output_img_dir = 'output_img/' + nowdate + "_" + os.path.basename(dicom_root_path)  # root 경로에 있는 디렉토리 이름으로 넣어줘야 함
 output_csv_dir = 'output_csv'   # root 경로에 있는 디렉토리 이름으로 넣어줘야 함
 
-def get_fieldnames():
+# def get_fieldnames():
+#     """
+#     여러곳에서 재 사용하기 위해 만든 함수
+#     뽑아야 할 다이콤 필드 리스트를 반환한다.
+#     :return: tag array
+#     """
+#     if all_or_one == 'y':
+#         dicom_tags = pd.read_csv('./dicom_CT_all.csv')
+#     else:
+#         dicom_tags = pd.read_csv('./dicom_CT_one.csv')
+#
+#     return ['directory_name'] + ['file_name'] + list(dicom_tags['Description'])
+
+
+def get_fieldnames_all():
     """
     여러곳에서 재 사용하기 위해 만든 함수
     뽑아야 할 다이콤 필드 리스트를 반환한다.
     :return: tag array
     """
-    dicom_tags = pd.read_csv('./dicom_CT_tags.csv')
+    dicom_tags = pd.read_csv('./dicom_CT_all.csv')
     return ['directory_name'] + ['file_name'] + list(dicom_tags['Description'])
 
 
-def folder_to_csv(folder_name):
+def get_fieldnames_one():
     """
-    폴더 안에 있는 다이콤 파일들을 읽어서 csv 로 빼는 함수
-    안쓸예정
-    :param folder_name:
-    :return: none
+    여러곳에서 재 사용하기 위해 만든 함수
+    뽑아야 할 다이콤 필드 리스트를 반환한다.
+    :return: tag array
     """
-    now = datetime.now()
-    folder_path = os.path.join(dicom_root_path, folder_name)
-    images_path = os.listdir(folder_path)
-    # Patient's information will be stored in working directory #'Patient_Detail_Info.csv'
-    with open(now.strftime("%y%m%d") + " " + folder_name + '.csv', 'w', newline='') as csvfile:
-        fieldnames = get_fieldnames()
-        writer = csv.writer(csvfile, delimiter=',')
-        writer.writerow(fieldnames)
-        for n, image in enumerate(images_path):
-            ds = dicom.dcmread(os.path.join(folder_path, image), force=True)
-            row = []
-            for field in fieldnames:
-                if field not in ds or ds.data_element(field) is None:
-                    row.append('')
-                else:
-                    x = str(ds.data_element(field)).replace("'", "")
-                    y = x.find(":")
-                    x = x[y + 2:]
-                    row.append(x)
-            writer.writerow(row)
+    dicom_tags = pd.read_csv('./dicom_CT_one.csv')
+    return ['directory_name'] + ['file_name'] + list(dicom_tags['Description'])
+
+# def folder_to_csv(folder_name):
+#     """
+#     폴더 안에 있는 다이콤 파일들을 읽어서 csv 로 빼는 함수
+#     안쓸예정
+#     :param folder_name:
+#     :return: none
+#     """
+#     now = datetime.now()
+#     folder_path = os.path.join(dicom_root_path, folder_name)
+#     images_path = os.listdir(folder_path)
+#     # Patient's information will be stored in working directory #'Patient_Detail_Info.csv'
+#     with open(now.strftime("%y%m%d") + " " + folder_name + '.csv', 'w', newline='') as csvfile:
+#         fieldnames = get_fieldnames()
+#         writer = csv.writer(csvfile, delimiter=',')
+#         writer.writerow(fieldnames)
+#         for n, image in enumerate(images_path):
+#             ds = dicom.dcmread(os.path.join(folder_path, image), force=True)
+#             row = []
+#             for field in fieldnames:
+#                 if field not in ds or ds.data_element(field) is None:
+#                     row.append('')
+#                 else:
+#                     x = str(ds.data_element(field)).replace("'", "")
+#                     y = x.find(":")
+#                     x = x[y + 2:]
+#                     row.append(x)
+#             writer.writerow(row)
 
 
 def folder_to_array(folder_name):
@@ -62,8 +97,8 @@ def folder_to_array(folder_name):
     folder_path = os.path.join(dicom_root_path, folder_name)
     images_path = os.listdir(folder_path)
 
-    rows = []
-    fieldnames = get_fieldnames()
+    rows_all = []
+    fieldnames = get_fieldnames_all()
     for n, image in enumerate(images_path):
         ds = dicom.dcmread(os.path.join(folder_path, image), force=True)
         row = []
@@ -77,12 +112,46 @@ def folder_to_array(folder_name):
                 y = x.find(":")
                 x = x[y + 2:]
                 row.append(x)
-        rows.append(row)
+        rows_all.append(row)
 
-    return rows
+    return rows_all
 
 
-def folder_to_jpg(input_folder_name,
+def file_to_array(folder_name):
+    """
+    폴더 안에 있는 다이콤 파일 중 하나를 (중간 위치) 읽어서 array 로 빼는 함수
+    :param folder_name:
+    :return: array
+    """
+    folder_path = os.path.join(dicom_root_path, folder_name)
+
+    dicom_files = os.listdir(folder_path)
+    dicom_files.sort()  # 이름순 정렬
+    select_image = dicom_files[int(len(dicom_files) / 2)]
+
+    row_one = []
+    fieldnames = get_fieldnames_one()
+    # for n, image in enumerate(images_path):
+    ds = dicom.dcmread(os.path.join(folder_path, select_image), force=True)
+
+    row = []
+    row.append(folder_name)
+    row.append(os.path.basename(folder_name))
+    for field in fieldnames:
+        if field not in ds or ds.data_element(field) is None:
+            row.append('')
+        else:
+            x = str(ds.data_element(field)).replace("'", "")
+            y = x.find(":")
+            x = x[y + 2:]
+            row.append(x)
+    row_one.append(row)
+
+    return row_one
+
+
+
+def folder_to_img(input_folder_name,
                   output_folder_name,
                   input_file_name=None,
                   output_file_name=None):
@@ -90,8 +159,8 @@ def folder_to_jpg(input_folder_name,
     폴더 안에 있는 다이콤 파일들을 읽어서 중간 다이콤 파일을 이미지로 출력
     폴더 안 특정 파일을 뽑아내고 싶을땐 파일 이름을 지정해 주면 됨
     e.g)
-    1. folder_to_jpg('dir01') => 폴더안 중간 파일
-    2. folder_to_jpg('dir01', '0045.dcm') => 폴더안 0045.dcm 파일을 jpeg 으로
+    1. folder_to_img('dir01') => 폴더안 중간 파일
+    2. folder_to_img('dir01', '0045.dcm') => 폴더안 0045.dcm 파일을 jpeg or png 으로
     :param input_folder_name: 다이콤파일들이 있는 디렉토리
     :param output_folder_name: 이미지 저장 장소
     :param input_file_name: None 이면 디폴트 파일을 읽고, 지정하면 지정 파일을 출력
@@ -108,14 +177,14 @@ def folder_to_jpg(input_folder_name,
         print("Directory does not exist. Creating %s" % output_folder_name)
         os.mkdir(output_folder_path)
 
-    default_jpeg_file_name = "%s.png" % input_folder_name
+    default_img_file_name = "%s.png" % input_folder_name
 
     # input_file_name 을 정했으면 그대로사용, 아니면 중간파일
     # 파일의 시작은 0, 마지막 파일은 int(len(dicom_files) - 1
     input_file_name = input_file_name or dicom_files[int(len(dicom_files) / 2)]
 
     # output_file_name 을 정했으면 그대로 사용, 아니면 디폴트
-    output_file_name = output_file_name or default_jpeg_file_name
+    output_file_name = output_file_name or default_img_file_name
 
     # path 설정
     input_file_path = os.path.join(input_folder_path, input_file_name)
@@ -126,12 +195,12 @@ def folder_to_jpg(input_folder_name,
 
     # 실제 변환
     try:
-        file_to_jpeg(input_file_path, output_file_path)
+        dicom_to_img(input_file_path, output_file_path)
     except:
         print("이미지변환 에러발생: %s" % output_file_name)
 
 
-def file_to_jpeg(input_file_path, output_file_path):
+def dicom_to_img(input_file_path, output_file_path):
     ds = dicom.dcmread(input_file_path, force=True)
     pixel_array = ds.pixel_array
     shape = pixel_array.shape
@@ -142,7 +211,7 @@ def file_to_jpeg(input_file_path, output_file_path):
     cv2.imwrite(output_file_path, image_2d_scaled_uint8)
 
 
-def file_to_csv(output_csv_dir):
+def move_csv(output_csv_dir):
     # 폴더 없으면 새로 생성
     if not os.path.exists(output_csv_dir):
         print("Directory does not exist. Creating %s" % output_csv_dir)
@@ -155,35 +224,64 @@ def file_to_csv(output_csv_dir):
 # main
 #
 print("Root dir is %s" % dicom_root_path)
-all_rows = []
-# 1. 전체 다 까서 한 배열에 집어 넣은 다음에
-for folder_name in os.listdir(dicom_root_path):  # [0:x] 0 부터 .. x 까지
-    if os.path.isdir(os.path.join(dicom_root_path, folder_name)) and folder_name != output_jpeg_dir:
-        # 디렉토리 이름을 출력해줘야 진행상황 알 수 있음
-        print("%s" % folder_name)
 
-        # 폴더별 jpeg 출력
-        # folder_to_jpg(folder_name, output_jpeg_dir)
+all_or_one = input('Extract information to all DCM files? (y or n): ')
+total_rows = []
 
-        # 데이터 추출 및 합치기
-        rows = folder_to_array(folder_name)
-        all_rows += rows
+if all_or_one == 'y':
+    # 1. 전체 다 까서 한 배열에 집어 넣은 다음에
+    for folder_name in os.listdir(dicom_root_path):  # [0:x] 0 부터 .. x 까지
+        if os.path.isdir(os.path.join(dicom_root_path, folder_name)) and folder_name != output_img_dir:
+            # 디렉토리 이름을 출력해줘야 진행상황 알 수 있음
+            print("%s" % folder_name)
 
-# 2. 합친 데이터를 csv 로 한꺼번에 출력 - 파일이름은 날짜_상위폴더명_all.csv
-outfile_name = datetime.now().strftime("%y%m%d") + "_" + os.path.basename(dicom_root_path) + "_all" + '.csv'
-with open(outfile_name, 'w', newline='') as csvfile:
-    # writer 생성
-    writer = csv.writer(csvfile, delimiter=',')
+            # 폴더별 jpeg 출력
+            folder_to_img(folder_name, output_img_dir)
 
-    # 헤더 출력
-    writer.writerow(get_fieldnames())
+            # 데이터 추출 및 합치기
+            rows = folder_to_array(folder_name)
+            total_rows += rows
 
-    # 데이터 출력
-    for row in all_rows:
-        writer.writerow(row)
+    # 2. 합친 데이터를 csv 로 한꺼번에 출력 - 파일이름은 날짜_상위폴더명_all.csv
+    outfile_name = nowdate + "_" + os.path.basename(dicom_root_path) + "_all" + '.csv'
+    with open(outfile_name, 'w', newline='') as csvfile:
+        # writer 생성
+        writer = csv.writer(csvfile, delimiter=',')
+
+        # 헤더 출력
+        writer.writerow(get_fieldnames_all())
+
+        # 데이터 출력
+        for row in total_rows:
+            writer.writerow(row)
+else:
+    for folder_name in os.listdir(dicom_root_path):  # [0:x] 0 부터 .. x 까지
+        if os.path.isdir(os.path.join(dicom_root_path, folder_name)) and folder_name != output_img_dir:
+            # 디렉토리 이름을 출력해줘야 진행상황 알 수 있음
+            print("%s" % folder_name)
+
+            # 폴더별 jpeg 출력
+            folder_to_img(folder_name, output_img_dir)
+
+            # 데이터 추출 및 합치기
+            rows = file_to_array(folder_name)
+            total_rows += rows
+
+    # 2. 합친 데이터를 csv 로 한꺼번에 출력 - 파일이름은 날짜_상위폴더명_all.csv
+    outfile_name = nowdate + "_" + os.path.basename(dicom_root_path) + "_one" + '.csv'
+    with open(outfile_name, 'w', newline='') as csvfile:
+        # writer 생성
+        writer = csv.writer(csvfile, delimiter=',')
+
+        # 헤더 출력
+        writer.writerow(get_fieldnames_one())
+
+        # 데이터 출력
+        for row in total_rows:
+            writer.writerow(row)
 
 # 3. csv 파일 경로 이동
-file_to_csv(output_csv_dir)
+move_csv(output_csv_dir)
 
 # 4. 하나만 출력할 경우 실행
 # folder_to_jpg("Thigh01-0867_DCM_POST", output_jpeg_dir)\
--- temp.py
+++
@@ -1,5 +0,0 @@
-import os
-
-
-str = '/Users/Yousun/Downloads/L3_External validation'
-print(os.path.basename(str))
